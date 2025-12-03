6fff4d5cc7330845f172f1ed501f40c9a4e2555a
Souptikbej <bejsouptik@gmail.com>
2025-12-03 23:32:53 +0530

    add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,13 +7,6 @@ import Notedetailspage from "./pages/Notedetailspage";
 const App = () => {
   return (
     <div>
-      <button className="btn btn-active">Default</button>
-      <button className="btn btn-active btn-neutral">Neutral</button>
-      <button className="btn btn-active btn-primary">Primary</button>
-      <button className="btn btn-active btn-secondary">Secondary</button>
-      <button className="btn btn-active btn-accent">Accent</button>
-      <button className="btn btn-active btn-ghost">Ghost</button>
-      <button className="btn btn-active btn-link">Link</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<Createpage />} />
--- Frontend/src/components/Navber.jsx
+++ Frontend/src/components/Navber.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-
+import { ListPlus } from "lucide-react";
 const Navber = () => {
   return (
     <header>
-      <div className="navbar bg-base-100">
-        <a className="btn btn-ghost text-xl">TaskApp</a>
-        <button className="btn btn-active btn-primary">Primary</button>
-      </div>
+      <nav class="bg-neutral px-10 py-4 shadow-lg">
+        <div class="container mx-auto flex items-center justify-between">
+          <a href="#" class="text-2xl font-bold text-white tracking-wider">
+            MyNotesApp
+          </a>
+          <button className="btn btn-active btn-primary">
+            <ListPlus />
+            <span class="hidden sm:inline">Add Note</span>
+            <span class="sm:hidden">New</span>
+          </button>
+        </div>
+      </nav>
     </header>
   );
 };
