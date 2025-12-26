124ba4c071e44b1711178856bcde67bff5cf5b92
Souptikbej <bejsouptik@gmail.com>
2025-12-26 21:44:07 +0300

    1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router";
-import HomePage from "./pages/Homepage";
-import Createpage from "./pages/Createpage";
-import NoteDetailsPage from "./pages/Notedetailspage";
-
-const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<Createpage />} />
-        <Route path="/note/:id" element={<NoteDetailsPage />} />
-      </Routes>
-    </div>
-  );
+const App = ({ children }) => {
+  return <div>{children}</div>;
 };
 
 export default App;
